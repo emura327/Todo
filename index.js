3421a98ea43401cfb68e7a67fa3c6902663a3464
EmuraRyota <emuryo.150327@ezweb.ne.jp>
2023-07-21 12:28:07 +0900

    upd

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const form = document.getElementById("form")
-const input = document.getElementById("input")
+const form = document.getElementById('form');
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
 
-form.addEventListener("submit",function () {
-    console.log(input.value)
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log(input.value);
+    add();
 });
+
+function add(){
+    const li = document.createElement("li");
+    li.innerText = input.value;
+    li.classList.add("list-group-item");
+}
